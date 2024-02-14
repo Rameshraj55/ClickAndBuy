5fd233120c502d99c54b24e3ddd8ad2de1728f61
Ramesh <mrrameshpvm@gmail.com>
2024-02-14 05:49:15 +0530

    Project initiated

---
+++ src/main/java/com/trailblazer/ClickAndBuy/ClickAndBuyApplication.java
@@ -0,0 +1,13 @@
+package com.trailblazer.ClickAndBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickAndBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickAndBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trailblazer/ClickAndBuy/ClickAndBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trailblazer.ClickAndBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickAndBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
